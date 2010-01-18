d768451a5e402406734347e6c1cd0eb113316c56
cina <cina@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-18 15:19:56 +0000

    fixed readme
    git-svn-id: https://plugins.svn.wordpress.org/wpcareers/trunk@194975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jp_control.php
+++ jp_control.php
@@ -43,7 +43,7 @@ if (!$table_prefix){$table_prefix = $wpdb->prefix;}
 /**
  * Constants
  */
-define('VERSION', '1.0-a');
+define('VERSION', '1.1-a');
 define('JP_PLUGIN_DIR', ABSPATH .  'wp-content/plugins/wpcareers');
 define('JP_PLUGIN_URL', plugins_url('wpcareers'));
 
